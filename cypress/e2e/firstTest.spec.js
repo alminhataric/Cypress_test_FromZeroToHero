7a87322bc0a75220960e377cceaa0ff70e913ef3
alminhataric <almin.hataric@edu.fit.ba>
2022-11-11 21:41:26 +0100

    Finding_Web_Elements

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,5 +1,6 @@
 ///<reference types="cypress" />
 
+
 describe('Our first suite', () => {
 
     it('first test', () => {
@@ -37,6 +38,31 @@ describe('Our first suite', () => {
 
         //The most recommended way by Cypress
         cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it.only('second test', () => {
+
+        cy.visit('http://localhost:4200/pages')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]','Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+            
+        cy.contains('nb-card','Horizontal form').find('[type="email"]')
+
+
     })
 
 })
