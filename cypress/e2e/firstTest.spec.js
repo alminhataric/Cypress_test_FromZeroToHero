203123fa1177a82b5dc126c17777a32e9646be44
alminhataric <almin.hataric@edu.fit.ba>
2022-11-13 14:27:30 +0100

    Invoke_command

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -65,7 +65,7 @@ describe('Our first suite', () => {
 
     })
 
-    it.only('then and wrap methods', () => {
+    it('then and wrap methods', () => {
 
         cy.visit('http://localhost:4200/pages')
         cy.contains('Forms').click()
@@ -101,6 +101,51 @@ describe('Our first suite', () => {
 
         })
 
+
+    })
+
+    it('invoke command', () => {
+
+        cy.visit('http://localhost:4200/pages')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2
+        cy.get('[for="exampleInputEmail1"]').then( label => {
+            expect(label.text() ).to.equal('Email address')
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            //.should('contain', 'checked')
+            .then(classValue => {
+                expect(classValue).to.contain('checked')
+            })
+
+    })
+
+    it.only('assert property', () => {
+
+        cy.visit('http://localhost:4200/pages')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            cy.wrap(input).invoke('prop', 'value').should('contains', 'Nov 17, 2022')
+        })
 
     })
 
