311a3df40b43077293aff5022974f64a3141f3d7
alminhataric <almin.hataric@edu.fit.ba>
2022-11-15 23:49:14 +0100

    Web_tables

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -245,5 +245,50 @@ describe('Our first suite', () => {
 
     })
 
+    it('Web table', () => {
+
+        cy.visit('http://localhost:4200/pages')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        // 1. Find table body and table row which contains name 'Larry' and change data 
+        cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        // 2 Add brand new value to the table
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then( tableRow => {
+            cy.wrap(tableRow).find('[placeholder= "First Name"]').type('Joe')
+            cy.wrap(tableRow).find('[placeholder= "Last Name"]').type('Doe')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+        })
+
+        cy.get('tbody tr').first().find('td').then( tableColumns => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'Joe')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Doe')
+        })
+
+        // 3 Search all data with same parameter
+        const age = [20, 30, 40, 200]
+
+        cy.wrap(age).each( age => {
+            cy.get('th [placeholder="Age"]').clear().type(age)
+            cy.wait(500)
+            cy.get('tbody tr').each( tableRow => {
+                if(age == 200){
+                    cy.wrap(tableRow).should('contain', 'No data found')
+                }
+                else{
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                }
+            })
+        
+        })
+    })
+
 })
 
