0c8cf958924ffa1832b13f9f3383724e441584f5
alminhataric <almin.hataric@edu.fit.ba>
2022-11-16 21:06:51 +0100

    web_Datepickers

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -138,14 +138,38 @@ describe('Our first suite', () => {
 
     it('assert property', () => {
 
+        // Use date object to get date and modify object to get needed values
+
+        function selectDayFromCurrent(day){
+
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleString('en-us', {month: 'short'})
+            let dateAssert = futureMonth+' '+futureDay+', '+date.getFullYear()
+
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+                if( !dateAttribute.includes(futureMonth)){
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } 
+                else {
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        }
+
         cy.visit('http://localhost:4200/pages')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
 
         cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
             cy.wrap(input).click()
-            cy.get('nb-calendar-day-picker').contains('17').click()
-            cy.wrap(input).invoke('prop', 'value').should('contains', 'Nov 17, 2022')
+            let dateAssert = selectDayFromCurrent(300)
+
+            cy.wrap(input).invoke('prop', 'value').should('contains', dateAssert)
         })
 
     })
@@ -289,6 +313,8 @@ describe('Our first suite', () => {
         
         })
     })
+
+
 
 })
 
