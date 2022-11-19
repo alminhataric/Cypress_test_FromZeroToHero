15619a1869a627214a35c7d851fdf95209d826e9
alminhataric <almin.hataric@edu.fit.ba>
2022-11-19 11:58:29 +0100

    end_To_end_Flow_With_Page_Objects

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,4 +1,7 @@
+import { onDatePickerPage } from "../support/page_objects/datepickerPage"
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
 import {navigateTo} from "../support/page_objects/navigationPage"
+import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 
 describe('Test with Page Objects', () => {
 
@@ -12,6 +15,21 @@ describe('Test with Page Objects', () => {
         navigateTo.smartTablePage()
         navigateTo.tooltipPage()
         navigateTo.toasterPage()
+
+    })
+
+    it('should submit InLine and Basic form and select tomorrow date in the calendar', () => {
+
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInLineFormWithNameAndEmail('James', 'test@test.com')
+        onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password')
+        navigateTo.datepickerPage()
+        onDatePickerPage.selectCommonDatepickerDateFromToday(1)
+        onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14)
+        navigateTo.smartTablePage()
+        onSmartTablePage.addNewRecordWithFirstAndLastName('John', 'Doe')
+        onSmartTablePage.updateAgeByFirstName('John', '25')
+        onSmartTablePage.deleteRowByIndex(1)
 
     })
 
